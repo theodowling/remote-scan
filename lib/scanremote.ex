2d9a3d3c942fa75ca8c594aa4c1a85d50b98e032
Theo Dowling <thdowling@gmail.com>
2023-06-14 19:40:28 +0200

    Add shell command to do the actual scanning

--- lib/scanremote.ex
+++ lib/scanremote.ex
@@ -3,10 +3,17 @@ defmodule Scanremote do
   Documentation for `Scanremote`.
   """
 
-  def call(%{path: "/"}),
-    do: %{code: 200, type: "text/plain", body: "OMG Y'ALL!"}
+  def call(%{path: "/scan/color", verb: "POST"}) do
+    System.shell("scanimage --progress --format png --mode color --resolution 300 > /opt/paperless-ngx/consume/image.png", into: IO.stream())
+    %{code: 200, type: "text/plain", body: "Scanned"}
+  end
+
+  def call(%{path: "/scan/gray", verb: "POST"}) do
+    System.shell("scanimage --progress --format png --mode gray --resolution 300 > /opt/paperless-ngx/consume/image.png", into: IO.stream())
+    %{code: 200, type: "text/plain", body: "Scanned"}
+  end
 
   # everything else is a 404 response
   def call(_),
-    do: %{code: 404, body: "say what now?"}
+    do: %{code: 404, body: "Not Found"}
 end
