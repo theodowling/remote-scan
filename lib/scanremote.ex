56aa9aec0d1baccc837489a0a0771fa9b4898f49
Theo Dowling <thdowling@gmail.com>
2023-06-15 21:26:00 +0200

    Name images uniquely

--- lib/scanremote.ex
+++ lib/scanremote.ex
@@ -4,13 +4,15 @@ defmodule Scanremote do
   """
 
   def call(%{path: "/scan/color", verb: "POST"}) do
-    System.shell("scanimage --progress --format png --mode color --resolution 300 > /opt/paperless-ngx/consume/image.png", into: IO.stream())
-    %{code: 200, type: "text/plain", body: "Scanned"}
+    doc_name = "image-#{System.system_time()}.png"
+    System.shell("scanimage --progress --format png --mode color --resolution 300 > /opt/paperless-ngx/consume/#{doc_name}", into: IO.stream())
+    %{code: 200, type: "application/json", body: "{outcome: \"Success\"}"}
   end
 
   def call(%{path: "/scan/gray", verb: "POST"}) do
-    System.shell("scanimage --progress --format png --mode gray --resolution 300 > /opt/paperless-ngx/consume/image.png", into: IO.stream())
-    %{code: 200, type: "text/plain", body: "Scanned"}
+    doc_name = "image-#{System.system_time()}.png"
+    System.shell("scanimage --progress --format png --mode gray --resolution 300 > /opt/paperless-ngx/consume/#{doc_name}", into: IO.stream())
+    %{code: 200, type: "application/json", body: "{outcome: \"Success\"}"}
   end
 
   # everything else is a 404 response
