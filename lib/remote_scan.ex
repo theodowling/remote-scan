dee42b40e62f3634fadf785a9ff486dd470d7c4a
Theo Dowling <thdowling@gmail.com>
2023-07-31 11:51:52 +0200

    Support lineart colour scanning also

--- lib/remote_scan.ex
+++ lib/remote_scan.ex
@@ -15,6 +15,12 @@ defmodule RemoteScan do
     %{code: 200, type: "application/json", body: "{outcome: \"Success\"}"}
   end
 
+  def call(%{path: "/scan/black-and-white", verb: "POST"}) do
+    doc_name = "image-#{System.system_time()}.png"
+    System.shell("scanimage --progress --format png --mode lineart --resolution 300 > /opt/paperless-ngx/consume/#{doc_name}", into: IO.stream())
+    %{code: 200, type: "application/json", body: "{outcome: \"Success\"}"}
+  end
+
   # everything else is a 404 response
   def call(_),
     do: %{code: 404, body: "Not Found"}
